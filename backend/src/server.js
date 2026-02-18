313a0bc3eca72f8bb89cc25b1130f04a1f5bac12
Duraimurugan-M <mdmurugan484@gmail.com>
2026-02-18 12:10:26 +0530

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Textile ERP API");
+});

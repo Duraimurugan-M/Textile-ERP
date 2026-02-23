8eb822784dd3e44b069b44471cfff307cadc6e5f
Duraimurugan-M <mdmurugan484@gmail.com>
2026-02-23 10:27:49 +0530

    Backend - Login details data must enter/required

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -44,6 +44,10 @@ export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
     const user = await User.findOne({ email }).populate("role");
 
     if (!user) {
